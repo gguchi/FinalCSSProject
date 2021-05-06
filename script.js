120cb131602e5d1e59b670363d02ce368babc4e8
james-99k <jamesgasty@gmail.com>
2021-05-06 20:56:59 +0800

    js letter grading

---
+++ script.js
@@ -0,0 +1,15 @@
+function assignGrade(score) {
+
+    if (score < 60){
+        console.log("F");
+    }else if (score < 70){
+        console.log("D");
+    }else if (score < 80){
+        console.log("C");
+    }else if (score < 90){
+        console.log("B");
+    }else if (score < 100){
+        console.log("A");
+    }
+    
+}
