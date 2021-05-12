89b972171731d6c18a0461bcb226ccc54d6e4322
Norimar Iguchi <gguchi193@gmail.com>
2021-05-12 21:05:55 +0800

    Updated js file

--- newjs.js
+++ newjs.js
@@ -2,13 +2,20 @@ let btnSubtract = document.querySelector(".counter__button--minus");
 let btnAdd = document.querySelector(".counter__button--plus");
 let number = document.querySelector(".counter__button--count");
 
-btnAdd.addEventListener('click', () =>{
+btnSubtract.style.cursor="pointer";
+btnAdd.style.cursor="pointer";
+
+btnAdd.addEventListener("click", add() =>{
     number.value = parseInt(number.value) + 1;
 });
-btnSubtract.addEventListener('click', () =>{
+
+btnSubtract.addEventListener("click", minus() =>{
     if (number.value >= 1){
         number.value = parseInt(number.value) - 1;
     } else {
         number.value = parseInt(number.value);
     }
 });
+
+console.log(number);
+alert(number);
