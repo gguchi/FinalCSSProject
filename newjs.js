cdc8b72aad5f896b0280a1b85b96ed0093b324db
Norimar Iguchi <gguchi193@gmail.com>
2021-05-17 21:04:48 +0800

    Added new images for product filter and new page for signing in

--- newjs.js
+++ newjs.js
@@ -1,15 +1,15 @@
 let btnSubtract = document.querySelector(".counter__button--minus");
 let btnAdd = document.querySelector(".counter__button--plus");
-let number = document.querySelector(".counter__button--count");
+let number = document.querySelector(".counter__button--counter");
 
-btnSubtract.style.cursor="pointer";
-btnAdd.style.cursor="pointer";
+btnSubtract.style.cursor = "pointer";
+btnAdd.style.cursor = "pointer";
 
-btnAdd.addEventListener("click", add() =>{
+btnAdd.addEventListener("click", () => {
     number.value = parseInt(number.value) + 1;
 });
 
-btnSubtract.addEventListener("click", minus() =>{
+btnSubtract.addEventListener("click", () =>{
     if (number.value >= 1){
         number.value = parseInt(number.value) - 1;
     } else {
@@ -17,5 +17,18 @@ btnSubtract.addEventListener("click", minus() =>{
     }
 });
 
-console.log(number);
-alert(number);
+// ---------------------- COLOR CHANGES
+
+let btnPink = document.querySelector(".circle__pink");
+let btnRed = document.getElementsByClassName(".circle__red");
+let btnBlack = document.document.getElementsByClassName(".circle__black");
+let btnWhite = document.getElementsByClassName(".circle__white");
+let btnBrown = document.getElementsByClassName(".circle__brown");
+
+btnPink.addEventListener("click", () => {
+
+    document.getElementsByClassName(".pic").src='images/beats_4.png';
+    alert("alert");
+
+});
+
